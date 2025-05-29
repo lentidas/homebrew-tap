6a337decd4696385c93e446e810b712753f17435
Gonçalo Heleno <goncalocheleno@atomp.eu>
2025-05-29 23:00:01 +0200

    feat: add hledger-csv-parser formula

---
+++ Formula/hledger-csv-parser.rb
@@ -0,0 +1,40 @@
+# typed: false
+# frozen_string_literal: true
+
+require "formula"
+require "../github-private-repository.rb"
+
+class HledgerCSVParser < Formula
+  desc "CLI tool to get market prices for commodities"
+  homepage "https://github.com/lentidas/hledger-csv-parser"
+  version "0.1.0"
+  license "GPL-3.0"
+
+  on_macos do
+    if Hardware::CPU.intel?
+      url "https://github.com/lentidas/hledger-csv-parser/releases/download/v0.1.0/hledger-csv-parser_Darwin_x86_64.tar.gz", :using => GitHubPrivateRepositoryReleaseDownloadStrategy 
+      sha256 "dc90ec9b3528f92c9ee2c91eed028ac9ccc6688acaed700cfd43a53bedcc9a9a"
+    end
+    if Hardware::CPU.arm?
+      url "https://github.com/lentidas/hledger-csv-parser/releases/download/v0.1.0/hledger-csv-parser_Darwin_arm64.tar.gz", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
+      sha256 "5d3f6b3fd053e20dffd25fe4c82abab588b177bcfea964af72ac312c86154894"
+    end
+  end
+
+  on_linux do
+    if Hardware::CPU.is_64_bit?
+      if Hardware::CPU.intel?
+        url "https://github.com/lentidas/hledger-csv-parser/releases/download/v0.1.0/hledger-csv-parser_Linux_x86_64.tar.gz", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
+        sha256 "0f524df55d8c08bc088b06f014258b1a54b6aabc40c8e20a253f580096e71c4c"
+      end
+      if Hardware::CPU.arm?
+        url "https://github.com/lentidas/hledger-csv-parser/releases/download/v0.1.0/hledger-csv-parser_Linux_arm64.tar.gz", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
+        sha256 "9c1111784a213037c12f0500ee373ab189c2d304135dec10ce2bc40fb43135f1"
+      end
+    end
+  end
+
+  def install
+    bin.install "hledger-csv-parser"
+  end
+end
