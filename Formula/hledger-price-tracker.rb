7c1fef56414e3a8a7d8f9ab0a8d73c7c1bdba1b8
Gonçalo Heleno <goncalocheleno@atomp.eu>
2025-07-26 17:30:54 +0200

    chore: update hledger-price-tracker to v0.1.9

--- Formula/hledger-price-tracker.rb
+++ Formula/hledger-price-tracker.rb
@@ -4,29 +4,29 @@
 class HledgerPriceTracker < Formula
   desc "CLI tool to get market prices for commodities"
   homepage "https://github.com/lentidas/hledger-price-tracker"
-  version "0.1.8"
+  version "0.1.9"
   license "GPL-3.0"
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/lentidas/hledger-price-tracker/releases/download/v0.1.8/hledger-price-tracker_Darwin_x86_64.tar.gz"
-      sha256 "4b78209a9dcb12555158e2a164b118a408199e8b778a94045b815d3f94ae8c1d"
+      url "https://github.com/lentidas/hledger-price-tracker/releases/download/v0.1.9/hledger-price-tracker_Darwin_x86_64.tar.gz"
+      sha256 "47061b671d58bb7deecf003ae1e0a75fa02dc21cc80292d1980510e16865a841"
     end
     if Hardware::CPU.arm?
-      url "https://github.com/lentidas/hledger-price-tracker/releases/download/v0.1.8/hledger-price-tracker_Darwin_arm64.tar.gz"
-      sha256 "86d7ab68a61ebc4f746fd6f4440b1a52c5f4fc8c90b0c570c098751c88fbdc06"
+      url "https://github.com/lentidas/hledger-price-tracker/releases/download/v0.1.9/hledger-price-tracker_Darwin_arm64.tar.gz"
+      sha256 "4a2f71ffc951b0649e5de6769289e297a06a32c9e324e4b40ab298c551a613bf"
     end
   end
 
   on_linux do
     if Hardware::CPU.is_64_bit?
       if Hardware::CPU.intel?
-        url "https://github.com/lentidas/hledger-price-tracker/releases/download/v0.1.8/hledger-price-tracker_Linux_x86_64.tar.gz"
-        sha256 "a148ae90993d1c69563bbea70937202641f02f8dbf69dcbe59079d2df8ffdc9e"
+        url "https://github.com/lentidas/hledger-price-tracker/releases/download/v0.1.9/hledger-price-tracker_Linux_x86_64.tar.gz"
+        sha256 "4c6cb6263f7e472d56598814bf686fb0b22301dafadc41256c20a5f5f800172a"
       end
       if Hardware::CPU.arm?
-        url "https://github.com/lentidas/hledger-price-tracker/releases/download/v0.1.8/hledger-price-tracker_Linux_arm64.tar.gz"
-        sha256 "d5540eb21f1fe78a73ac38adae3a223b62b16e30e9c492f4d83e64d4529c409d"
+        url "https://github.com/lentidas/hledger-price-tracker/releases/download/v0.1.9/hledger-price-tracker_Linux_arm64.tar.gz"
+        sha256 "a9401f33ed8d0a5597c916d0eabe16a85af4ce72e21265f2b0e0f9acf767a2e8"
       end
     end
   end
